8fabdd048212efe33602880a340635a6bacf55f9
sefaria <lorenzo@>
2020-09-29 16:18:36 -0400

    Add pytest parallel
    Working CI builds
    
    Move secret into /.secrets
    
    Working creation and delete
    
    Test pytest in GHA
    
    Get pytest running. Additional work on Selenium-Sauce

---
+++ reader/browsertest/framework/creds.py
@@ -0,0 +1,7 @@
+# Use environment variables instead
+import os
+
+BS_USER = os.getenv("BS_USER")
+BS_KEY = os.getenv("BS_KEY")
+SAUCE_USERNAME = os.getenv("SAUCE_USERNAME")
+SAUCE_ACCESS_KEY = os.getenv("SAUCE_ACCESS_KEY")
