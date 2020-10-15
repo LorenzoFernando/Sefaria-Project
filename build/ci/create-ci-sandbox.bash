87189e2a7702562049f7d63ab2dfa0fe541ca682
sefaria <lorenzo@>
2020-10-15 00:49:28 -0400

    Shorten git commit hash

--- build/ci/create-ci-sandbox.bash
+++ build/ci/create-ci-sandbox.bash
@@ -105,8 +105,8 @@ resourceAllocation="small"
 # Create Cloud Builder variables
 substVars=()
 substVars+=("_RESOURCE_ALLOCATION=$resourceAllocation")
-substVars+=("_ENV_NAME=$gitCommit")
-substVars+=("_GIT_COMMIT=$gitCommit")
+substVars+=("_ENV_NAME=${gitCommit:0:8}")
+substVars+=("_GIT_COMMIT=${gitCommit:0:8}")
 substVars+=("_GIT_REPO=$gitRepoName")
 substVars+=("_GKE_CLUSTER=$gkeCluster")
 substVars+=("_GKE_NAMESPACE=$gkeNamespace")
